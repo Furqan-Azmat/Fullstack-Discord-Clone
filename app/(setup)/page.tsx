5f7d8bae3e79d8816303a05d1a49a2332cd31c50
Furqan-Azmat <furqanazmat@gmail.com>
2024-01-08 17:18:47 -0500

    Basic server name creation

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,7 @@
 import { intialProfile } from "@/lib/intital-profile";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
+import { InitialModal } from "@/components/modals/intital-modal";
 
 const SetupPage = async () => {
     const profile = await intialProfile();
@@ -19,7 +20,7 @@ const SetupPage = async () => {
         return redirect(`/servers/${server.id}`)
     }
 
-    return <div>Create a Server</div>;
+    return <InitialModal/>;
 }
  
 export default SetupPage;
